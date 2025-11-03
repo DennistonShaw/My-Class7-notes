461b6b38d9039994e4c88a3ad7aa1218bd7a257c
DennistonShaw <232101875+DennistonShaw@users.noreply.github.com>
2025-11-03 07:41:56 -0500

    week 8 working terraform folder

---
+++ 11-01-25_wk8-pt1/2-subnets.tf
@@ -0,0 +1,85 @@
+########################## Public subnets ##########################
+
+
+resource "aws_subnet" "public_a" {
+  vpc_id     = aws_vpc.main.id 
+  # step 1: make VPC
+  # step 2: get VPC ID
+  # step 3: add VPC ID into vpc_id argument in this subnet 
+  # which VPC to create subnet in
+  
+  cidr_block = "10.71.1.0/24" 
+  availability_zone = "us-east-1a" # specify AZ 
+  map_public_ip_on_launch = true   # allow public IPs to be assigned 
+
+  tags = {
+    Name = "public-subnet-us-east-1a"
+  }
+}
+
+
+resource "aws_subnet" "public_b" {
+  vpc_id     = aws_vpc.main.id 
+  cidr_block = "10.71.2.0/24" 
+  availability_zone = "us-east-1b" # specify AZ 
+  map_public_ip_on_launch = true   # allow public IPs to be assigned 
+
+  tags = {
+    Name = "public-subnet-us-east-1b"
+  }
+}
+
+
+resource "aws_subnet" "public_c" {
+  vpc_id     = aws_vpc.main.id 
+  cidr_block = "10.71.3.0/24" 
+  availability_zone = "us-east-1c" # specify AZ 
+  map_public_ip_on_launch = true   # allow public IPs to be assigned 
+
+  tags = {
+    Name = "public-subnet-us-east-1c"
+  }
+}
+
+
+
+
+
+
+
+########################## Private subnets ##########################
+
+resource "aws_subnet" "private_a" {
+  vpc_id     = aws_vpc.main.id 
+  cidr_block = "10.71.11.0/24" 
+  availability_zone = "us-east-1a" # specify AZ 
+  map_public_ip_on_launch = false  
+
+  tags = {
+    Name = "private-subnet-us-east-1a"
+  }
+}
+
+
+resource "aws_subnet" "private_b" {
+  vpc_id     = aws_vpc.main.id 
+  cidr_block = "10.71.12.0/24" 
+  availability_zone = "us-east-1b" # specify AZ 
+  map_public_ip_on_launch = false   
+  
+  tags = {
+    Name = "private-subnet-us-east-1b"
+  }
+}
+
+
+resource "aws_subnet" "private_c" {
+  vpc_id     = aws_vpc.main.id 
+  cidr_block = "10.71.13.0/24" 
+  availability_zone = "us-east-1c" # specify AZ 
+  map_public_ip_on_launch = false   
+
+  tags = {
+    Name = "private-subnet-us-east-1c"
+  }
+}
---
+++ 11-01-25_wk8-pt1/notes.md
@@ -0,0 +1,36 @@
+class 7 (11-01-25)
+week 8 (Terraform pt 3) catch up, SG, and EC2
+
+goal = craft your own network
+architecture, create a VPC, and create an EC2 withing the VPC, all in Terraform
+
+*******************************
+
+VPC (Virginia) = 10.71.0.0/16
+                 us-east-1
+
+Public A = 10.71.1.0/24
+Public B = 10.71.2.0/24
+Public C = 10.71.3.0/24
+Public D = 10.71.4.0/24
+
+Private A = 10.71.11.0/24
+Private B = 10.71.12.0/24
+Private C = 10.71.13.0/24
+Private D = 10.71.14.0/24
+
+IVPAD
+    terraform init
+    terraform validate
+    terraform plan
+    terraform apply
+    terraform destroy
+
+Push to my repository
+    echo "# My-Class7-notes" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/DennistonShaw/My-Class7-notes.git
+git push -u origin main
